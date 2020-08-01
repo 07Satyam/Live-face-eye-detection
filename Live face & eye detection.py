58c0db77b86621b6a91bf577476c2b44cedf154b
Satyam Gadekar <60726090+07Satyam@users.noreply.github.com>
2020-08-01 16:46:10 +0530

    Add files via upload

---
+++ Live face & eye detection.py
@@ -0,0 +1,31 @@
+#Import Libraries 
+import numpy as np
+import cv2
+import os
+os.chdir('E:\Github Repo\Face & eye detection using Haar Cascade classifier')
+
+# We point OpenCV's CascadeClassifier function to where our 
+# classifier (XML file format) is stored
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_classifier = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img=cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+         roi_gray = gray[y:y+h, x:x+w]
+         roi_color = img[y:y+h, x:x+w]
+         eyes = eye_classifier.detectMultiScale(roi_gray)
+   
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    cv2.imshow('img_Detection',img)
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+               break
+    
+cap.release()    
+cv2.destroyAllWindows()    
